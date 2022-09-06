05b861f8298542befef71a4000efc9b7e0f9d45f
Tugdual Saunier <tugdual.saunier@gmail.com>
2022-09-06 16:38:51 -0400

    Perf optimization: precompute CrudController FQCN
    This commit prevents computing all (and loading the classes) this on every single request (because we are in an event listener), even if this is not an admin request.

--- src/Registry/CrudControllerRegistry.php
+++ src/Registry/CrudControllerRegistry.php
@@ -2,8 +2,6 @@
 
 namespace EasyCorp\Bundle\EasyAdminBundle\Registry;
 
-use EasyCorp\Bundle\EasyAdminBundle\Contracts\Controller\CrudControllerInterface;
-
 /**
  * @author Javier Eguiluz <javier.eguiluz@gmail.com>
  */
@@ -15,20 +13,17 @@ final class CrudControllerRegistry
     private $crudIdToCrudFqcnMap = [];
 
     /**
-     * @param CrudControllerInterface[] $crudControllers
+     * @param string[] $crudFqcnToEntityFqcnMap
+     * @param string[] $crudFqcnToCrudIdMap
+     * @param string[] $crudIdToCrudFqcnMap
+     * @param string[] $entityFqcnToCrudFqcnMap
      */
-    public function __construct(string $kernelSecret, array $crudControllersFqcn)
+    public function __construct(array $crudFqcnToEntityFqcnMap, array $crudFqcnToCrudIdMap, array $entityFqcnToCrudFqcnMap, array $crudIdToCrudFqcnMap)
     {
-        foreach ($crudControllersFqcn as $controllerFqcn) {
-            $this->crudFqcnToEntityFqcnMap[$controllerFqcn] = $controllerFqcn::getEntityFqcn();
-            $this->crudFqcnToCrudIdMap[$controllerFqcn] = substr(sha1($kernelSecret.$controllerFqcn), 0, 7);
-        }
-
-        // more than one controller can manage the same entity, so this map will
-        // only contain the last controller associated to that repeated entity. That's why
-        // several methods in other classes allow to define the CRUD controller explicitly
-        $this->entityFqcnToCrudFqcnMap = array_flip($this->crudFqcnToEntityFqcnMap);
-        $this->crudIdToCrudFqcnMap = array_flip($this->crudFqcnToCrudIdMap);
+        $this->crudFqcnToEntityFqcnMap = $crudFqcnToEntityFqcnMap;
+        $this->crudFqcnToCrudIdMap = $crudFqcnToCrudIdMap;
+        $this->entityFqcnToCrudFqcnMap = $entityFqcnToCrudFqcnMap;
+        $this->crudIdToCrudFqcnMap = $crudIdToCrudFqcnMap;
     }
 
     public function findCrudFqcnByEntityFqcn(string $entityFqcn): ?string
